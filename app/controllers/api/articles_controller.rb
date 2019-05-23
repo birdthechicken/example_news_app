e4219d74f87020a52829802db506123c8a4f49d0
birdthechicken <sinangundogar61@gmail.com>
2019-05-23 17:07:13 -0500

    week5 day3

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,12 @@
 class Api::ArticlesController < ApplicationController
   def index
     response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ENV['API_KEY']}")
-    render json: response.parse
+   
+
+    data = response.parse
+    title = data["articles"][0]["title"]
+    description = data["articles"][0]["description"]
+
+    render json: {title: title, description: description}
   end
 end
